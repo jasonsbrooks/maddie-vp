d6ba0d1b6e62bf2970bea7f9243844aec2487a5c
Jason Brooks <jason.brooks@yale.edu>
2015-04-05 18:11:14 -0400

    Final fixes

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
 $(document).ready(function() { 
   var scroll_start = 0;
-  // var startchange = $('.heroText1');
-  // var offset = startchange.offset();
+  $('#logo').hide();
   var offset = $(window).height() / 3;
   $(document).scroll(function() { 
      scroll_start = $(this).scrollTop();
      if(scroll_start > offset) {
          $('.navbar').addClass('scrolled');
-         // $('#logo').attr('src', 'static/img/logo-blk.png');
+         $('#logo').show();
+         $('#large-logo').fadeOut();
       } else {
          $('.navbar').removeClass('scrolled');
-         // $('#logo').attr('src', 'static/img/logo.png');
+         $('#logo').hide();
+         $('#large-logo').fadeIn();
       }
   });
 
